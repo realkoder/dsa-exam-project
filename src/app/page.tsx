077c4f3425c1c71d7a84018b1eaf8fd20c8797ca
mikk39k1 <mikk39k1@stud.kea.dk>
2024-06-05 13:13:52 +0200

    animations added, and gamemenu added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
-import { TicTacToePage } from "@/templates/TicTacToePage";
+"use client";
 
+import GameMenuPage from "@/templates/GameMenuPage";
+import { TicTacToePage } from "@/templates/TicTacToePage";
+import { useState } from "react";
 
 export default function Home() {
+  const [difficulty, setDifficulty] = useState<number>(0);
+  const [backToMenu, setBackToMenu] = useState<boolean>(false);
+
+  // Handle going back to the menu
+  const handleGoBackToMenu = () => {
+    setBackToMenu(true);
+    setDifficulty(0); 
+  };
+
   return (
-    <TicTacToePage />
+    <>
+      {backToMenu || difficulty === 0 ? (
+        <GameMenuPage difficulty={difficulty} setDifficulty={(diff) => {
+          setDifficulty(diff);
+          setBackToMenu(false);
+        }} />
+      ) : (
+        <TicTacToePage difficulty={difficulty} handleGoBackToMenu={handleGoBackToMenu} />
+      )}
+    </>
   );
 }
